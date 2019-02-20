db33d037ae1dbbf58d2c32589317e874da1d13c5
Artem-work <artem-work@mail.ru>
2019-02-20 18:05:32 +0300

    'modded'

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
 from config import TOKEN, PROXY, TEXT
 
 from aiogram import Bot, Dispatcher, executor, types
+import asyncio
 import logging
 
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=TOKEN, proxy=PROXY)
+loop = asyncio.get_event_loop()
+bot = Bot(token=TOKEN, loop=loop,parse_mode=types.ParseMode.MARKDOWN, proxy=PROXY)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply(TEXT['START'])
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(message):
+    await message.reply(TEXT['HELP'])
+
+
+@dp.inline_handler()
+async def inline_def(inline_query):
+    gi_list = ['HUB', 'TGK']
+    input_contents = [types.InputContactMessageContent(f"/g_def {i}") for i in gi_list]
+    item1 = types.InlineQueryResultArticle(id='1', title='🛡 HUB', input_message_content=input_contents[0])
+    item2 = types.InlineQueryResultArticle(id='2', title='🛡 TGK', input_message_content=input_contents[1])
+    await bot.answer_inline_query(inline_query.id, results=[item1], cache_time=1)
+    await bot.answer_inline_query(inline_query.id, results=[item2], cache_time=1)
 
 
 if __name__ == '__main__':
