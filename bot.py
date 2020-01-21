cb9bbd7bff93c535f1cc51c3122b8bee2ad909f5
ArtemYarovenko <artik3386@gmail.com>
2020-01-22 00:32:28 +0300

    add echo json

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.environ.get('PORT')
 TOKEN = os.environ.get('TOKEN')
-CHAT_ID = os.environ.get('CHAT_ID')
+CHAT_ID = int(os.environ.get('CHAT_ID'))
 CHAT_WARS_BOT_ID = 265204902
 
 # logging.basicConfig(level=logging.DEBUG)
@@ -71,13 +71,14 @@ async def send_def_message(message: types.Message):
     await send_def(CHAT_ID, get_def_msg(), keyboard)
 
 
+# обработка сообщения от биржи в игре
 @dp.message_handler(lambda message: message.text
                     and message.text.startswith('Здесь ты можешь купить и продать разные ресурсы.')
-                    )  # and message.chat.id == CHAT_ID) -1001245540882
+                    and message.chat.id == int(CHAT_ID)
+                    and message.forward_from.id == CHAT_WARS_BOT_ID)
 async def send_all_message(msg: types.Message):
     print(msg.chat.id, type(msg.chat.id))
     print(msg.forward_from.id, type(msg.forward_from.id))
-    print(msg.text)
     await msg.reply('Спасибо. Биржа сохранена.')
 
 
