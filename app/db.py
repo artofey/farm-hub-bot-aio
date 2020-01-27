b974ded2a80bb71d3c5dd009673f78a3f330b859
ArtemYarovenko <artik3386@gmail.com>
2020-01-28 00:15:35 +0300

    fix connect to db

--- app/db.py
+++ app/db.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DB_CONNECT = 'postgresql://postgres:1@localhost/farm_bot'
+# DB_CONNECT = 'postgresql://postgres:1@localhost/farm_bot'
+DB_CONNECT = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DB_CONNECT, echo=True)
 Base = declarative_base()
